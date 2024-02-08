f98a50d8f318c129a42de333fc6dffb4f61cd960
sinhong jeong <jsh719803@gmail.com>
2024-02-08 15:11:46 +0900

    feat: 게시글 작성 API 업데이트
    - 게시글 작성 API에 카테고리별 권한 체크 로직 추가
    - PROMOTION 카테고리에 대한 작성 권한이 없는 사용자의 요청을 거부하고 403 상태 코드를 반환하도록 수정

--- src/router/documents.router.js
+++ src/router/documents.router.js
@@ -3,3 +3,28 @@ import { prisma } from '../utils/prisma/index.js';
 import authMiddleware from '../middleware/auth.middleware.js';
 
 const router = express.Router();
+
+// 게시글 작성 API
+router.post('/posts', authMiddleware, async (req, res, next) => {
+  const { title, content, category } = req.body;
+  const { userId, role } = req.user;
+
+  try {
+    if (category === 'PROMOTION' && role !== 'admin') {
+      return res
+        .status(403)
+        .json({ message: 'PROMOTION 카테고리에 작성할 권한이 없습니다.' });
+    }
+    const post = await prisma.posts.create({
+      data: {
+        userId: +userId,
+        title: title,
+        content: content,
+        category: category,
+      },
+    });
+    return res.status(201).json({ data: post });
+  } catch (error) {
+    next(error);
+  }
+});
