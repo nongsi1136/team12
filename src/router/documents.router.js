1d5613ce424c2a3888c7d0edcbf1a8ccd3179be7
sinhong jeong <jsh719803@gmail.com>
2024-02-08 15:38:18 +0900

    feat : 이미지를 게시글에 추 - 이미지를 서버에 업로드하고 해당 이미지의 경로를 데이터베이스에 저장

--- src/router/documents.router.js
+++ src/router/documents.router.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 import authMiddleware from '../middleware/auth.middleware.js';
+import { error } from 'winston';
 
 const router = express.Router();
 
 // 게시글 작성 API
 router.post('/posts', authMiddleware, async (req, res, next) => {
-  const { title, content, category } = req.body;
+  const { title, content, category, imageUrl } = req.body;
   const { userId, role } = req.user;
 
   try {
@@ -21,9 +22,36 @@ router.post('/posts', authMiddleware, async (req, res, next) => {
         title: title,
         content: content,
         category: category,
+        imageUrl: imageUrl,
       },
     });
     return res.status(201).json({ data: post });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//게시글 상세 조회 API >> 내 글만 확인할 수 있는 피드
+router.get('/posts/:userId', async (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    const myPost = await prisma.posts.findUnique({
+      where: { userId: parseInt(userId) },
+      orderBy: { createdAt: prisma.SortOrder.desc }, // 최신 게시글이 먼저 표시됩니다.
+      select: {
+        postId: true,
+        title: true,
+        content: true,
+        imageUrl: true,
+        user: {
+          select: { name: true },
+        },
+        createdAt: true,
+        category: category,
+      },
+    });
+    return res.status(200).json({ data: myPost });
   } catch (error) {
     next(error);
   }
